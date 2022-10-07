130a7b0c76e97fef8cfe8688ace8b12749b18c38
LubovMalina <andreeva.lubov27@gmail.com>
2022-10-07 16:17:52 +0300

    Цифры введенные с клавиатуры, счет больше 0

---
+++ HomeWork06/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+Console.WriteLine("Введите количество чисел, которые вы введете: ");
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+
+for (int i = 0; i < number; i++)
+{
+    Console.WriteLine("Введите число: ");
+    int number1 = int.Parse(Console.ReadLine());
+    if (number1>0)
+    {
+        count+=1;
+    }
+}
+Console.WriteLine($"Вы ввели {count} чисел больше 0");
